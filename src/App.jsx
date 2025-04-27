df0e67fcf31f6e011fcea454471719367eda1cb2
thuy <thuyly1312@gmal.com>
2025-04-27 13:51:02 +0700

    hello

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { logoreact } from './assets/react.svg'
+import './Components/todo/todo.css'
+const App = () => {
+  return (
+    <>
+      <div className="todo-container">
+        <div className="todo-title">Todo List</div>
+        <div className="todo-data">
+          <input type="text" />
+          <button>Add</button>
+        </div>
+        <div>Learning React</div>
+      </div>
+    </>
+  )
+}
+
+export default App
